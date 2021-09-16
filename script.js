715cd238e3c05ae36c88d4acc3991e62db8d38a2
Kylychev <janat.kylychev.kk@gmail.com>
2021-09-16 22:29:03 +0400

    Done

---
+++ script.js
@@ -0,0 +1,32 @@
+let userName ;
+
+let lastName;
+
+let userAge;
+
+let adress ;
+
+userName =prompt('Your Name');
+lastName =prompt("Your last Name");
+userAge =prompt('Your age');
+adress = prompt('Your city')
+
+document.write("<b> What is your name?  </b>" + userName +"<br>" + "<b> What is your last name?  </b>"+lastName+"<br>"+"<b>How old are ? </b> "+userAge+"<br> " +"<b>Where are you live? </b> "+ adress);
+
+
+
+
+
+
+
+
+
+//let number1 = 200;
+
+//let number2 = 2021;
+
+//document.write(number1+number2);
+
+//const pi =3.14;
+//document.write( pi+0.01)
+
